460e574d0ea742de170d546328b6efa340bd2ff1
Jane <jane666bender@gmail.com>
2021-03-25 23:38:05 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class Controller {
+    final String USERNAME = "Jane";
+    @FXML
+    TextArea textArea;
+    @FXML
+    TextField textField;
+    @FXML
+    void sendMsg() {
+        textArea.appendText(USERNAME + ": " + textField.getText() + '\n');
+        textField.clear();
+        textField.requestFocus();
+    }
+        }
---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.awt.event.InputEvent;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("ChatFX");
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+        public static void main (String[]args){
+            launch(args);
+        }
+    }
